5fedf0245064669c8be4f225249fca009775ecb2
romainx <romainx@users.noreply.github.com>
2020-12-18 17:23:58 +0100

    Using qemu-user-static in persistent mode

--- build_miniforge.sh
+++ build_miniforge.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Check parameters
 ARCH=${ARCH:-aarch64}
-DOCKER_ARCH=${DOCKER_ARCH:arm64v8}
+DOCKER_ARCH=${DOCKER_ARCH:-arm64v8}
 DOCKERIMAGE=${DOCKERIMAGE:-condaforge/linux-anvil-aarch64}
 export CONSTRUCT_ROOT=/construct
 
@@ -13,18 +13,16 @@ mkdir -p build/
 chmod 777 build/
 
 echo "============= Enable QEMU ============="
-docker run --rm --privileged multiarch/qemu-user-static:register --reset --credential yes
+docker run --rm --privileged multiarch/qemu-user-static --reset --credential yes --persistent yes
 
 echo "============= Build the installer ============="
 docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT -e MINIFORGE_VERSION -e MINIFORGE_NAME $DOCKERIMAGE /construct/scripts/build.sh
 
 echo "============= Download QEMU static binaries ============="
-bash scripts/get_qemu.sh
+#bash scripts/get_qemu.sh
 
 echo "============= Test the installer ============="
-for TEST_IMAGE_NAME in "ubuntu:20.04" "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"
-do
+for TEST_IMAGE_NAME in "ubuntu:20.04" "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"; do
   echo "============= Test installer on $TEST_IMAGE_NAME ============="
-  docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT -v $(pwd)/build/qemu/qemu-${ARCH}-static:/usr/bin/qemu-${ARCH}-static ${DOCKER_ARCH}/$TEST_IMAGE_NAME /construct/scripts/test.sh
+  docker run --rm -v $(pwd):/construct -e CONSTRUCT_ROOT ${DOCKER_ARCH}/$TEST_IMAGE_NAME /construct/scripts/test.sh
 done
-
